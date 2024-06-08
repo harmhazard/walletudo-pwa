96fa0d8b8f9a949d507adebcff5e0eacb2139c59
dan <dnbsd@posteo.net>
2024-06-08 13:12:46 +0200

    Update ConnectPage

--- src/pages/ConnectPage.vue
+++ src/pages/ConnectPage.vue
@@ -1,47 +1,38 @@
 <template>
   <q-page class="items-center justify-evenly col">
     <div class="q-gutter-y-md column col-auto" style="max-width: 600px; margin:auto;">
-
-      <q-input filled v-model="url" label="Url address" class="" />
-      <q-input filled v-model="user" label="Login" class="" />
-      <q-input filled v-model="subject" label="Subject" class="" />
-      <q-input filled v-model="wallet" label="Wallet name" class="" />
+      <q-input filled v-model="url" label="URL" class="" />
+      <q-input filled v-model="user" label="User" class="" />
       <q-input filled v-model="password" label="Password" class="" type="password" />
+
       <div>
         <q-btn push color="primary" label="Connect" @click="connectFn"/>
-        <q-btn push color="primary" label="Disconnect" class='float-right' @click="disconnectFn"/>
       </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
+import {ref} from 'vue'
+import {NatsError} from "nats.ws"
+import {newNatsStore} from "stores/natsStore"
 
+const natsStore = newNatsStore()
+const url = ref(natsStore.getCredentials.url)
+const user = ref(natsStore.getCredentials.user)
+const password = ref(natsStore.getCredentials.password)
 
-import {computed, ref} from 'vue';
-import {natsStore} from "stores/natsStore";
+async function connectFn(){
+  try {
+    await natsStore.connect(url, user, password)
+  } catch(NatsError) {
+    console.log("error connecting to nats server")
+    return
+  }
+  console.log("connected!")
+}
 
 defineOptions({
   name: 'ConnectPage'
 });
-
-const countStore = natsStore();
-function connectFn(){
-  countStore.connect(user, password, url, subject, wallet);
-}
-
-function disconnectFn() {
-  console.log(countStore.getCredentials);
-}
-const url = ref('');
-const user = ref('');
-const subject = ref('');
-const wallet = ref('');
-const password = ref('');
-
-
-
-
-
-
 </script>
